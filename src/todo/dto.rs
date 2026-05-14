c2ca30efa7e1aa508dcfa30247a84b6327dcde58
Amsterdan Vasconcelos <amsterdan.rust@gmail.com>
2026-05-14 08:10:59 -0300

    feat: add todo http dtos

--- src/todo/dto.rs
+++ src/todo/dto.rs
@@ -1,3 +1,21 @@
+use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+use super::domain::Todo;
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodoRequest {
+    pub title: String,
+    pub description: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateTodoRequest {
+    pub title: Option<String>,
+    pub description: Option<Option<String>>,
+}
+
 #[derive(Debug)]
 pub struct CreateTodoInput {
     pub title: String,
@@ -8,4 +26,45 @@ pub struct CreateTodoInput {
 pub struct UpdateTodoInput {
     pub title: Option<String>,
     pub description: Option<Option<String>>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct TodoResponse {
+    pub id: Uuid,
+    pub title: String,
+    pub description: Option<String>,
+    pub completed: bool,
+    pub created_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+}
+
+impl From<CreateTodoRequest> for CreateTodoInput {
+    fn from(request: CreateTodoRequest) -> Self {
+        Self {
+            title: request.title,
+            description: request.description,
+        }
+    }
+}
+
+impl From<UpdateTodoRequest> for UpdateTodoInput {
+    fn from(request: UpdateTodoRequest) -> Self {
+        Self {
+            title: request.title,
+            description: request.description,
+        }
+    }
+}
+
+impl From<Todo> for TodoResponse {
+    fn from(todo: Todo) -> Self {
+        Self {
+            id: todo.id().as_uuid(),
+            title: todo.title().as_str().to_string(),
+            description: todo.description().as_deref().map(str::to_string),
+            completed: todo.completed(),
+            created_at: todo.created_at(),
+            updated_at: todo.updated_at(),
+        }
+    }
 }
