34912eaf5619a24f28868552ecef4de1b488d944
Amsterdan Vasconcelos <amsterdan.rust@gmail.com>
2026-05-14 07:27:08 -0300

    feat: add todo repository port

---
+++ src/todo/repository.rs
@@ -0,0 +1,78 @@
+use std::{collections::HashMap, sync::Arc};
+
+use async_trait::async_trait;
+use tokio::sync::RwLock;
+
+use super::{
+    domain::{Todo, TodoId},
+    error::TodoError,
+};
+
+#[async_trait]
+pub trait TodoRepository: Send + Sync {
+    async fn create(&self, todo: Todo) -> Result<Todo, TodoError>;
+
+    async fn find_all(&self) -> Result<Vec<Todo>, TodoError>;
+
+    async fn find_by_id(&self, id: TodoId) -> Result<Option<Todo>, TodoError>;
+
+    async fn update(&self, todo: Todo) -> Result<Todo, TodoError>;
+
+    async fn delete(&self, id: TodoId) -> Result<(), TodoError>;
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct InMemoryTodoRepository {
+    todos: Arc<RwLock<HashMap<TodoId, Todo>>>,
+}
+
+impl InMemoryTodoRepository {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+#[async_trait]
+impl TodoRepository for InMemoryTodoRepository {
+    async fn create(&self, todo: Todo) -> Result<Todo, TodoError> {
+        let mut todos = self.todos.write().await;
+
+        todos.insert(todo.id(), todo.clone());
+
+        Ok(todo)
+    }
+
+    async fn find_all(&self) -> Result<Vec<Todo>, TodoError> {
+        let todos = self.todos.read().await;
+
+        Ok(todos.values().cloned().collect())
+    }
+
+    async fn find_by_id(&self, id: TodoId) -> Result<Option<Todo>, TodoError> {
+        let todos = self.todos.read().await;
+
+        Ok(todos.get(&id).cloned())
+    }
+
+    async fn update(&self, todo: Todo) -> Result<Todo, TodoError> {
+        let mut todos = self.todos.write().await;
+
+        if !todos.contains_key(&todo.id()) {
+            return Err(TodoError::NotFound);
+        }
+
+        todos.insert(todo.id(), todo.clone());
+
+        Ok(todo)
+    }
+
+    async fn delete(&self, id: TodoId) -> Result<(), TodoError> {
+        let mut todos = self.todos.write().await;
+
+        if todos.remove(&id).is_none() {
+            return Err(TodoError::NotFound);
+        }
+
+        Ok(())
+    }
+}
